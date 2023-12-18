6193cd789df6874e9f3d5fce3658e7d9df11d3be
Dima Danilchenko <switch57dima@yandex.ru>
2023-12-18 22:20:00 +0300

    start frontend

--- src/components/Promo/Promo.js
+++ src/components/Promo/Promo.js
@@ -3,6 +3,13 @@ import './Promo.css';
 
 export default function Promo() {
   return (
-    <div>Promo</div>
+    <div className='promo'>
+      <p className="promo__title">Учебный проект студента факультета Веб-разработки.</p>
+      <div className="promo__buttons">
+        <button className="promo__button">О проекте</button>
+        <button className="promo__button">Технологии</button>
+        <button className="promo__button">Студент</button>
+      </div>
+    </div>
   )
 }
